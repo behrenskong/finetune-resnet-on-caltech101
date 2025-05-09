534234ef699cbdf47f2743242038c26b8f8386f5
Michael-Yi-Rong <rongyi8848@foxmail.com>
2025-05-09 21:01:53 +0800

    初次提交：上传项目并忽略 data 文件夹

---
+++ test.sh
@@ -0,0 +1,6 @@
+python test.py \
+    --config configs/resnet34_finetune.yaml \
+    --model_path outputs/checkpoints/resnet34_finetune/checkpoint_best.pth \
+    --experiment_name resnet34_finetune \
+    --gpu 0 \
+    --output_dir outputs/test_reports
---
+++ train.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+# # distributed training
+# python -m torch.distributed.launch \
+#     --nproc_per_node=4 \
+#     --master_port=29500 \
+#     train.py \
+#     --config configs/resnet34_finetune.yaml \
+#     --experiment_name resnet34_finetune \
+#     --gpu 0,1,2,3 \
+#     --distributed
+
+
+# # multi gpu training
+# python train.py \
+#     --config configs/resnet34_finetune.yaml \
+#     --experiment_name resnet34_finetune \
+#     --gpu 0,1,2,3
+
+
+
+# single gpu training
+python train.py \
+    --config configs/resnet34_finetune.yaml \
+    --experiment_name resnet34_finetune \
+    --gpu 0
