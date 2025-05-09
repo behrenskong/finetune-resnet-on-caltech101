a0ba83dade11d8eefb325f837f18d02223c4232c
Michael-Yi-Rong <rongyi8848@foxmail.com>
2025-05-09 21:36:31 +0800

    Merge remote-tracking branch 'origin/main'

--- test.sh
+++ test.sh
@@ -1,8 +1,4 @@
-<<<<<<< HEAD
 python test.py \
-=======
-python src/test.py \
->>>>>>> f7f9787a2692ff13254e0a0624755bacd57832d4
     --config configs/resnet34_finetune.yaml \
     --model_path outputs/checkpoints/resnet34_finetune/checkpoint_best.pth \
     --experiment_name resnet34_finetune \
